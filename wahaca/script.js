90e24def258ef0b8cf84c3f038156786aed6f1be
alexismalzieu <alex.malzieu@gmail.com>
2021-01-25 19:18:06 +0100

    💄 add mobile menu

--- wahaca/script.js
+++ wahaca/script.js
@@ -1,8 +1,14 @@
-window.onscroll = function () {
-  var logo = document.getElementsByClassName('mainLogo')[0]
-  var header = document.getElementsByClassName('header')[0]
-  var headerWrapper = document.getElementsByClassName('headerWrapper')[0]
+const logo = document.getElementsByClassName('mainLogo')[0]
+const header = document.getElementsByClassName('header')[0]
+const headerWrapper = document.getElementsByClassName('headerWrapper')[0]
+const smMenuWrapper = document.getElementsByClassName('smMenuWrapper')[0]
+const menuIconContainer = document.getElementsByClassName(
+  'menuIconContainer'
+)[0]
+const burgerIcon = document.getElementsByClassName('burgerIcon')[0]
+const closeIcon = document.getElementsByClassName('closeIcon')[0]
 
+window.onscroll = function () {
   if (window.pageYOffset > 100) {
     logo.classList.add('--scroll')
     header.classList.add('--scroll')
@@ -12,4 +18,23 @@ window.onscroll = function () {
     header.classList.remove('--scroll')
     headerWrapper.classList.remove('--scroll')
   }
+}
+
+let isOpen = false
+
+const toggleMenu = function () {
+  isOpen = !isOpen
+}
+
+const setToogleMenuClass = function () {
+  if (isOpen) {
+    smMenuWrapper.classList.add('--isOpen')
+  } else {
+    smMenuWrapper.classList.remove('--isOpen')
+  }
+}
+
+menuIconContainer.onclick = function () {
+  toggleMenu()
+  setToogleMenuClass()
 }
