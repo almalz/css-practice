2b3e0385e0ab0993a36fd2495273ac0f8c018672
almalz <alex.malzieu@gmail.com>
2021-05-13 23:11:04 +0200

    ✨ add slider hero

---
+++ nandos/src/script.js
@@ -0,0 +1,11 @@
+var glideHero = new Glide('.hero', {
+  type: 'carousel',
+  animationDuration: 2000,
+  autoplay: 94500,
+  focusAt: '1',
+  startAt: 1,
+  perView: 1,
+  gap: 0,
+})
+
+glideHero.mount()
